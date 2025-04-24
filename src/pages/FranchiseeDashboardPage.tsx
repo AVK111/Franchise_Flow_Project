bc307af6621f40d80d6668f48e4a997d4e3ebd0c
HEIRARCH <adityasirsat1234@gmail.com>
2025-04-24 09:56:50 +0530

    V4

--- src/pages/FranchiseeDashboardPage.tsx
+++ src/pages/FranchiseeDashboardPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/FranchiseeDashboard.css';
 import { Button } from "@/components/ui/button";
+import { useAuth } from '../contexts/AuthContext';
 
 // Type definitions
 interface Application {
